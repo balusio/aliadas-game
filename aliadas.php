913ecc2cef300b4115f92b857b7b8cf7c6a9cc35
Balu Manzano <jorgemanzanoweb@gmail.com>
2018-05-21 00:50:58 -0300

    added cron function

--- aliadas.php
+++ aliadas.php
@@ -39,7 +39,8 @@ class aliadasGame {
 		          user_id mediumint(9) NOT NULL,
 		          user_score mediumint(9) NOT NULL,
 		          user_rankposs mediumint(9),
-		          user_time varchar(255),
+		          user_start datetime,
+		          user_end datetime,
 		          UNIQUE KEY id (id)
 		     ) $charset_collate;";
 		     require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
@@ -51,15 +52,19 @@ class aliadasGame {
 	public static function checkUserState($user_id){
 		$table_name= aliadasGame::$db->prefix.'gameScore';
 		$current_user = aliadasGame::$db->get_row( "SELECT * FROM $table_name WHERE user_id = $user_id" );
-		
+		$timeStart = date("Y-m-d H:i:s");
+
 		if($current_user){
-			print_r($current_user);
+			
+			$upadteScore = aliadasGame::$db->query( "UPDATE $table_name set user_start = '". $timeStart ."' WHERE user_id = " . $user_id );
 		}
 		else{
 			//REGISTER GAME
+			
 			$added_user = aliadasGame::$db->insert($table_name, array(
 			    'user_id' => $user_id,
-			    'user_score' => 200
+			    'user_score' => 200,
+			    'user_start' => $timeStart
 			));
 
 			echo $added_user . "AGREGADO EL USUARIO";
@@ -69,12 +74,13 @@ class aliadasGame {
 
 	}
 
-	public static function updateUserScore($user_id,$timePassed){
+	public static function updateUserScore($user_id){
 
 		$table_name= aliadasGame::$db->prefix.'gameScore';
-		$score= 350;
-		$upadteScore = aliadasGame::$db->query( "UPDATE $table_name set user_score = user_score + ". $score . ", user_time = '". $timePassed ."' WHERE user_id = " . $user_id );
-		print_r($upadteScore);
+		$timePassed = date("Y-m-d H:i:s");
+		$score= 0;
+		$upadteScore = aliadasGame::$db->query( "UPDATE $table_name set user_score = user_score + ". $score . ", user_end = '". $timePassed ."' WHERE user_id = " . $user_id );
+		/*print_r($upadteScore);*/
 	}
 
 	public static function checkExchange($user_id,$price){
@@ -97,6 +103,21 @@ class aliadasGame {
 
 
 
+
+	}
+
+	public static function monthlyUpdate(){
+		$values = [1000,800,600,500,400,300,200,100,100,100];
+		$table_name= aliadasGame::$db->prefix.'gameScore';
+		$lasts_users = aliadasGame::$db->get_results( "SELECT user_id, user_score, TIMESTAMPDIFF(SECOND,user_start,user_end) as TIMEDOWN FROM $table_name ORDER BY TIMEDOWN ASC LIMIT 10");	
+		
+		$i = 0;
+		foreach ($lasts_users as $current_user) {	
+
+			$upadteScore = aliadasGame::$db->query( "UPDATE $table_name set user_score = user_score + ". $values[$i] . " WHERE user_id = " . $current_user->user_id);
+
+			$i++;
+		}
 
 	}
 
